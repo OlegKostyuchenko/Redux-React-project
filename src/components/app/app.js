4004b8d1fa57e6b3b567b0561b3b8e10ef2cf722
Oleg <mr.lost@rambler.ru>
2022-07-05 22:50:27 +0300

    Start setings for project

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import {MainPage, CartPage} from '../pages';
+import { MainPage, CartPage } from '../pages';
 import AppHeader from '../app-header';
+import WithRestoService from '../hoc'
 
 import Background from './food-bg.jpg';
 
-const App = () => {
+const App = ({ RestoService }) => {
+    console.log(RestoService.getMenuItems());
     return (
-        <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
+        <div style={{ background: `url(${Background}) center center/cover no-repeat` }} className="app">
+            <AppHeader total={50} />
+            <MainPage />
+            <CartPage />
         </div>
     )
 }
 
-export default App;
+export default WithRestoService()(App);
